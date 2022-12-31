bc538877fddfb01fcc3ec9d126ce719f921152b5
kosar <kosar.jaff@gmail.com>
2022-12-30 19:09:28 -0800

    Update getkey.py

--- src/nostr/py3/getkey.py
+++ src/nostr/py3/getkey.py
@@ -1,6 +1,9 @@
 from nostr.key import PrivateKey
 
+# Create a new key
 private_key = PrivateKey()
+
+# The public key hangs off the key
 public_key = private_key.public_key
 print(f"Private key: {private_key.bech32()}")
 print(f"Public key: {public_key.bech32()}")

5dca4f01ecadaf4d2e0bb069228dc8dc913719d0
kosar <kosar.jaff@gmail.com>
2022-12-30 19:08:39 -0800

    Update connectrelay.py

--- src/nostr/py3/connectrelay.py
+++ src/nostr/py3/connectrelay.py
@@ -7,7 +7,7 @@ relay_manager = RelayManager()
 relay_manager.add_relay("wss://nostr-pub.wellorder.net")
 relay_manager.add_relay("wss://relay.damus.io")
 relay_manager.open_connections({"cert_reqs": ssl.CERT_NONE}) # NOTE: This disables ssl certificate verification
-time.sleep(1.25) # allow the connections to open
+time.sleep(1.25) # todo: measure how long it takes to make this smaller (waiting for connection to settle)
 
 while relay_manager.message_pool.has_notices():
   notice_msg = relay_manager.message_pool.get_notice()

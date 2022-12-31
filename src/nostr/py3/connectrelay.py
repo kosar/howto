77d261400e9070a741984c3c961b59f6e0afdc6f
kosar <kosar.jaff@gmail.com>
2022-12-30 19:02:12 -0800

    Add python clients for pub & read
    Tested on raspberry pi4

---
+++ src/nostr/py3/connectrelay.py
@@ -0,0 +1,16 @@
+import json
+import ssl
+import time
+from nostr.relay_manager import RelayManager
+
+relay_manager = RelayManager()
+relay_manager.add_relay("wss://nostr-pub.wellorder.net")
+relay_manager.add_relay("wss://relay.damus.io")
+relay_manager.open_connections({"cert_reqs": ssl.CERT_NONE}) # NOTE: This disables ssl certificate verification
+time.sleep(1.25) # allow the connections to open
+
+while relay_manager.message_pool.has_notices():
+  notice_msg = relay_manager.message_pool.get_notice()
+  print(notice_msg.content)
+  
+relay_manager.close_connections()

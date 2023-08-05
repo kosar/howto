ce07cc6978f3c1d9ed6c1942525fbb8ca903ebc7
kosar <kosar.jaff@gmail.com>
2023-08-05 14:29:03 -0700

    Update internet_watcher.py
    Add ability to detect a deviation from a moving avg

--- internet_watcher.py
+++ internet_watcher.py
@@ -54,12 +54,45 @@ def run_speed_test():
         if download_line and upload_line:
             download_speed = float(download_line.split()[1])
             upload_speed = float(upload_line.split()[1])
-            return download_speed, upload_speed
+            # Get the current timestamp
+            timestamp = datetime.datetime.now()
+            return timestamp, download_speed, upload_speed
         else:
             raise ValueError("Speed test output not found")
     except Exception as e:
         logger.warning(f"Error during speed test: {e}")
+        return None, None, None
+
+
+def calculate_moving_average(filename, num_records):
+    try:
+        with open(filename, 'r', newline='') as file:
+            reader = csv.DictReader(file)
+            download_speeds = []
+            upload_speeds = []
+            
+            for row in reader:
+                download_speeds.append(float(row['Download Speed (Mbps)']))
+                upload_speeds.append(float(row['Upload Speed (Mbps)']))
+                
+            download_average = sum(download_speeds[-num_records:]) / len(download_speeds[-num_records:])
+            upload_average = sum(upload_speeds[-num_records:]) / len(upload_speeds[-num_records:])
+            
+            return download_average, upload_average
+    except Exception as e:
+        logger.warning(f"Error calculating moving average: {e}")
         return None, None
+
+def check_alert_threshold(download_speed, upload_speed, download_avg, upload_avg, threshold_percentage):
+    # Calculate the percentage distance between the latest speed and the moving average
+    download_distance = abs((download_speed - download_avg) / download_avg * 100)
+    upload_distance = abs((upload_speed - upload_avg) / upload_avg * 100)
+
+    # Check if the distance exceeds the threshold percentage
+    if download_distance > threshold_percentage or upload_distance > threshold_percentage:
+        logger.warning(f"Speeds deviated significantly from moving average. Download Distance: {download_distance:.2f}%, Upload Distance: {upload_distance:.2f}%")
+        # Add any additional action here, such as sending an email or triggering an alert system.
+
 
 def write_header_if_not_exists(filename):
     if not os.path.exists(filename):
@@ -76,7 +109,8 @@ def write_to_csv(filename, data):
     except Exception as e:
         logger.error(f"Error writing to CSV: {filename}, Error: {e}")
 
-def main(interval):
+
+def main(interval, moving_average_records, threshold_percentage):
     script_directory = os.getcwd()
     csv_filename = os.path.join(script_directory, f"{os.path.splitext(os.path.basename(__file__))[0]}.csv")
 
@@ -85,15 +119,12 @@ def main(interval):
     logger.info("Interval: %s seconds", interval)
 
     while True:
-        # Get the current time and date
-        now = datetime.datetime.now()
-
-        # Run the speed test
-        download_speed, upload_speed = run_speed_test()
+        # Run the speed test and get the timestamp, download speed, and upload speed
+        timestamp, download_speed, upload_speed = run_speed_test()
 
         if download_speed is not None and upload_speed is not None:
             # Create a list with the results and timestamp
-            data = [now.strftime('%Y-%m-%d %H:%M:%S'), download_speed, upload_speed]
+            data = [timestamp.strftime('%Y-%m-%d %H:%M:%S'), download_speed, upload_speed]
 
             # Write the results to the CSV file
             write_to_csv(csv_filename, data)
@@ -101,6 +132,18 @@ def main(interval):
             # Emit log messages with the file location and test speed
             logger.info(f"Internet Speed Test - Date: {data[0]}, Download Speed: {data[1]} Mbps, Upload Speed: {data[2]} Mbps")
             print(f"Internet Speed Test - Date: {data[0]}, Download Speed: {data[1]} Mbps, Upload Speed: {data[2]} Mbps")
+
+            # Calculate the moving average
+            download_avg, upload_avg = calculate_moving_average(csv_filename, moving_average_records)
+
+            if download_avg is not None and upload_avg is not None:
+                # Check if the latest speed test results deviate from the average by more than the threshold percentage
+                download_diff = abs(download_speed - download_avg) / download_avg * 100
+                upload_diff = abs(upload_speed - upload_avg) / upload_avg * 100
+
+                if download_diff > threshold_percentage or upload_diff > threshold_percentage:
+                    logger.warning(f"Significant deviation from moving average - Download: {download_diff:.2f}%, Upload: {upload_diff:.2f}%")
+                    # You can add your alerting mechanism here
         else:
             logger.warning("Speed test failed. Skipping CSV update.")
             print ("Speed test failed. Skipping CSV update.")
@@ -114,6 +157,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Run internet speed test and log the results in a CSV file.')
     parser.add_argument('--interval', type=int, default=3600, help='Interval in seconds for the speed test loop.')
+    parser.add_argument('--moving-average-records', type=int, default=5, help='Number of records to consider for moving average calculation.')
+    parser.add_argument('--threshold-percentage', type=float, default=10, help='Threshold percentage for alert generation.')
     args = parser.parse_args()
 
     # Use the script's directory to create the CSV file
@@ -122,4 +167,4 @@ if __name__ == "__main__":
     # Write the header row if the file does not exist
     write_header_if_not_exists(csv_filename)
 
-    main(args.interval)
+    main(args.interval, args.moving_average_records, args.threshold_percentage)

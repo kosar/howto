8be4fd8dd02e25a174d37c0b6d3fc02d93736dd2
kosar <kosar.jaff@gmail.com>
2023-08-07 11:55:25 -0700

    Add CSV attachment to email alerts - internet_watcher.py
    Now the CSV is attached so it is easy to see the historical results for further processing.

--- internet_watcher.py
+++ internet_watcher.py
@@ -11,6 +11,9 @@ import logging.handlers
 import os
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 def load_env_vars(file_path):
     """Load environment variables from a shell script file."""
@@ -27,8 +30,7 @@ def log_env_vars():
     for var_name, var_value in os.environ.items():
         logger.info(f"Environment Variable: {var_name}={var_value}")
 
-def send_email(subject, body):
-    """Send an email using the configured SMTP settings."""
+def send_email(subject, body, attachment_path=None):
     sender_email = os.environ.get("SENDER_EMAIL")
     receiver_email = os.environ.get("RECEIVER_EMAIL")
     smtp_server = os.environ.get("SMTP_SERVER")
@@ -36,20 +38,36 @@ def send_email(subject, body):
     smtp_username = os.environ.get("SMTP_USERNAME")
     smtp_password = os.environ.get("SMTP_PASSWORD")
 
-    msg = MIMEText(body)
-    msg['Subject'] = subject
+    msg = MIMEMultipart()
     msg['From'] = sender_email
     msg['To'] = receiver_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    if attachment_path:
+        with open(attachment_path, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+        encoders.encode_base64(part)
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {os.path.basename(attachment_path)}",
+        )
+
+        msg.attach(part)
 
     try:
         with smtplib.SMTP(smtp_server, smtp_port) as server:
             server.starttls()
             server.login(smtp_username, smtp_password)
             server.sendmail(sender_email, receiver_email, msg.as_string())
+            logger.info("Email sent successfully.")
         print("Email sent successfully.")
     except Exception as e:
         print(f"Error sending email: {e}")
-
+        logger.info(f"Error sending email: {e}")
 
 # Generate LOG_FILENAME dynamically based on the current folder and script name
 LOG_FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"{os.path.splitext(os.path.basename(__file__))[0]}.log")
@@ -181,6 +199,9 @@ def main(interval, moving_average_records, threshold_percentage):
                     logger.warning(f"Significant deviation from moving average - Download: {download_diff:.2f}%, Upload: {upload_diff:.2f}%")
                     # You can add your alerting mechanism here
                     try:
+                        # Get the CSV file path to attach it to the email
+                        csv_file_path = os.path.join(script_directory, f"{os.path.splitext(os.path.basename(__file__))[0]}.csv")
+
                         # Include the interesting metric in the email subject
                         subject = f"Speed Alert - D:{download_diff:.2f}% U:{upload_diff:.2f}%"
                         
@@ -196,7 +217,7 @@ def main(interval, moving_average_records, threshold_percentage):
                         max_upload = max(upload_values[-moving_average_records:])
                         
                         message_body = f"D: {download_speed:.2f} Mbps (Max: {max_download:.2f} Mbps), U: {upload_speed:.2f} Mbps (Max: {max_upload:.2f} Mbps)\n"
-                        send_email(subject, message_body)
+                        send_email(subject, message_body, attachment_path=csv_file_path)
                     except Exception as e:
                         logger.warning(f"Error during email alert: {e}")
 
